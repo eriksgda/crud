35fd5ba0958a9c6f34ed235096c8fed004d713db
eriksgda <erik.ksgda@gmail.com>
2024-05-02 20:32:01 -0300

    add updata_func with json

--- functions/update.py
+++ functions/update.py
@@ -1,18 +1,22 @@
+import json
+from crud.functions.json.read_json import readJson
 
-def update(choice_update:int, data_dict:dict):
+def update(choice_update:int, choice:int, dict_choice:dict):
+  file_name = dict_choice[choice].lower()
+  datas_json = readJson(file_name)
   try:
     choice_code = int(input('Digite o código do aluno que deseja alterar: '))
   except:
     return print('Valor inválido!')
   
   values = [] # valores de cada estudante
-  for i in data_dict['estudantes']:
+  for i in datas_json:
     values  += i.values()
 
   if choice_code not in values: # verificação se o código existe
     return print('Valor Não Encontrado!')
 
-  for i in data_dict['estudantes']:
+  for i in datas_json:
     if i['codigo'] == choice_code:
       if choice_update == 0: # verificação se vai alterar o nome
         print()
@@ -20,6 +24,9 @@ def update(choice_update:int, data_dict:dict):
         update_ = input('Novo nome:').title()
         if update_ not in values:
           i['nome'] = update_
+          with open(file_name + '.json', 'w') as file:
+            json.dump(datas_json, file, ensure_ascii=False)
+            file.close()
           break
         else:
           print('Nome já incluso nos dados!')
@@ -33,6 +40,9 @@ def update(choice_update:int, data_dict:dict):
           return print('Valor Inválido!')
         if update_ not in values:
           i['cpf'] = update_
+          with open(file_name + '.json', 'w') as file:
+            json.dump(datas_json, file, ensure_ascii=False)
+            file.close()
           break
         else:
           print('CPF já incluso nos dados!')
