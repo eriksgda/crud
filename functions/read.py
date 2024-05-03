b26c18cf08c15d4772ccbb57bbac92d64b1009b5
eriksgda <erik.ksgda@gmail.com>
2024-05-02 22:08:12 -0300

    update read and creat func

--- functions/read.py
+++ functions/read.py
@@ -1,8 +1,32 @@
 from crud.functions.json.read_json import readJson
 
 def read(choice:int, dict_choice:dict):
-  file_name = dict_choice[choice].lower()
-  data_json = readJson(file_name)
-  print(f'--------------- [{dict_choice[choice]}] ---------------')
-  for student in data_json:
-    print(f'{student['codigo']:<5}{student['nome']:^30}{student['cpf']:<5}')
+  if choice != 5 and choice != 4 and choice !=3:
+    file_name = dict_choice[choice].lower()
+    data_json = readJson(file_name)
+    print(f'--------------- [{dict_choice[choice]}] ---------------')
+    print('Codigo              Nome              CPF')
+    for x in data_json:
+      print(f'{x['codigo']:<5}{x['nome']:^30}{x['cpf']:<5}')
+  elif choice == 3:
+    file_name = dict_choice[choice].lower()
+    data_json = readJson(file_name)
+    print(f'--------------- [{dict_choice[choice]}] ---------------')
+    print('Codigo                           Nome')
+    for x in data_json:
+      print(f'{x['codigo']:<5}{x['nome']:^30}')
+  elif choice == 4:
+    file_name = dict_choice[choice].lower()
+    data_json = readJson(file_name)
+    print(f'--------------- [{dict_choice[choice]}] ---------------')
+    print('Cod.               Cod.             Cod.\n'
+          'Turma           Professores        Disciplinas')
+    for x in data_json:
+      print(f'{x['codigo_c']:<5}{x['codigo_t']:^30}{x['codigo_d']:<5}')
+  elif choice == 5:
+    file_name = dict_choice[choice].lower()
+    data_json = readJson(file_name)
+    print(f'--------------- [{dict_choice[choice]}] ---------------')
+    print('Cod. Turmas              Cod. Estudantes')
+    for x in data_json:
+      print(f'{x['codigo_c']:<5}{x['codigo_s']:^30}')
